c9fa4650576e868a8a0c9b05697419c9b2fd96ad
rrrorrigo <rodrigobriones@gmail.com>
2021-02-03 17:05:01 +0000

    programming C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  * puts print exactly "Programming is like building a multilingual puzzle
@@ -5,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
