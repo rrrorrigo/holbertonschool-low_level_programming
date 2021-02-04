3dbfc59e2fd753acb3f1b6dc428180329eb627f5
rrrorrigo <rodrigobriones@gmail.com>
2021-02-04 20:02:36 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	int ii = 0;
+
+	for (; ii <= 9; ii++)
+	{
+		for (; i<=9; i++)
+		{
+			putchar('0' + ii);
+			putchar('0' + i);
+			putchar(',');
+			putchar(' ');
+		}
+		i=0;
+	}
+	putchar('\n');
+
+	return (0);
+}
