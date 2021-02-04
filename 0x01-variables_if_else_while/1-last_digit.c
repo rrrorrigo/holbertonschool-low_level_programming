29c799b9e2518bf928973edfb0ad9f3fb7dcc7ed
rrrorrigo <rodrigobriones@gmail.com>
2021-02-04 13:51:55 +0000

    Project: 0x01

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * generate  random number and print the last digit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int i = n % 10;
+
+	char a[15] = "Last digit of";
+
+	char b[30] = "and is less than 6 and not 0";
+
+	if (i == 0)
+	{
+		printf("%s %d is %d and is zero\n", a, n, i);
+	}
+	else
+	{
+		if (i < 6)
+		{
+			printf("%s %d is %d %s\n", a, n, i, b);
+		}
+		else
+		{
+			printf("%s %d is %d and is greater than 5\n", a, n, i);
+		}
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+	for (ch = 'a' ; ch <= 'Z' ; ch++)
+	{
+		if ()
+		putchar(ch);
+	}
+
+	return (0);
+}
