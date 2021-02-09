b606c3884f325c23a3e78d8d13e575459fe3eca8
rrrorrigo <rodrigobriones@gmail.com>
2021-02-09 16:44:25 +0000

    Project: 0x02. C

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_last_digit - check the code.
+ *
+ * @c: external value that was check
+ *
+ * Return: the last digit.
+ */
+
+int print_last_digit(int c)
+{
+	int ld;
+
+	if (c < 0)
+	{
+		c = (-1) * c;
+	}
+	ld = c % 10;
+	_putchar('0' + ld);
+	return (ld);
+}
