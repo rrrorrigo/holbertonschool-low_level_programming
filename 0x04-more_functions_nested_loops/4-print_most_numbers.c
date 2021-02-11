f0930145da441903af9a232912b288178eb8a847
rrrorrigo <rodrigobriones@gmail.com>
2021-02-11 15:53:37 +0000

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_most_numbers - print numbers from 0 to 9 except 2 and 4.
  *
@@ -9,10 +10,15 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 0; a++)
+	for (a = 0; a < 10; a++)
 	{
-		if (a != 2 || a != 4)
+		if (a == 2 || a == 4)
+		{
+		}
+		else
+		{
 			_putchar('0' + a);
+		}
 	}
 	_putchar('\n');
 }
