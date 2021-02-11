3026ce98a6550dd3e67f1ba0d979e29c59bafe02
rrrorrigo <rodrigobriones@gmail.com>
2021-02-11 13:23:50 +0000

    Project: 0x04. C more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Description: print the numbers from 1 to 100
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for(; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
