5ef772e514621e6fd7e541a27e485305fc6904b8
rrrorrigo <rodrigobriones@gmail.com>
2021-02-16 20:02:38 +0000

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse.
+ * @s: external value
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int length;
+
+	for (length = 0; s[length] != 0; length++)
+	{
+	}
+
+	for (i = length; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
