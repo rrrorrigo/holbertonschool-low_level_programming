7b4576ef9453a5fdae84dde7ebf00ef87e7d3f0f
rrrorrigo <rodrigobriones@gmail.com>
2021-02-16 23:36:53 +0000

    please checker

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print string.
+ * @a: external value
+ * @n: external value
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", a[i]);
+		}
+		else
+			printf("%d, ", a[i]);
+	}
+}
