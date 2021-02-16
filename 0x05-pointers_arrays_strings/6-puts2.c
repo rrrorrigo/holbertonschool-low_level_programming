f7a1dc3336f06c3c887ed8618be7c3e38e5d9d6a
rrrorrigo <rodrigobriones@gmail.com>
2021-02-16 23:09:37 +0000

    checker ugly

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,12 @@ void puts2(char *str)
 {
 	int length;
 
-	for (length = 0; str[length] != 0; length++)
+	for (length = 0; str[length] != '\0'; length++)
 	{
 	}
-	for (int i = 0; i < length;)
+	for (int i = 0; i < length; i += 2)
 	{
 		_putchar(str[i]);
-		i = i + 2;
 	}
 	_putchar('\n');
 }
