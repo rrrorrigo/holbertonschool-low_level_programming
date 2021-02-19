ecff105f6cb0c8774fcab59ad7fb03e1324d3dfc
rrrorrigo <rodrigobriones@gmail.com>
2021-02-19 22:47:09 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,25 +12,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, len, len2;
 
-	for (len = 0; s1[len] != '\0'; len++)
-	{
-	}
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-	{
-	}
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[i] && len > len2)
+		if (s1[i] != s2[i])
 		{
-			return (15);
-		}
-		else if (s1[i] != s2[i] && len2 > len)
-		{
-			return (-15);
-		}
-		else if (s1[i] != s2[i] && len == len2)
-		{
-			return (0);
+			return (*s1 - *s2);
 		}
 		i++;
 	}
