d4ccfd2dac73970f1f70732066106e67e04dae95
rrrorrigo <rodrigobriones@gmail.com>
2021-02-19 19:05:05 +0000

    Project: 0x06 C

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <string.h>
+
 /**
  * _strcat - updates the value to 98.
  * @dest: external value
@@ -9,9 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = strlen(dest), len2 = strlen(src);
+	int i, len, len2;
 
-	for (int i = 0; i < (len + len2); i++)
+	for (len = 0; dest[len] != '\0'; len++)
+	{
+	}
+	for (len2 = 0; src[len2] != '\0'; len2++)
+        {
+        }
+	for (i = 0; i < (len + len2); i++)
 		dest[i + len] = src[i];
 	dest[len + len2] = 0;
 	return (dest);
