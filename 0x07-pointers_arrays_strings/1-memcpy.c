3ba61af0bafa9092c7636507e5589052cba85883
rrrorrigo <rodrigobriones@gmail.com>
2021-02-23 13:25:30 +0000

    pointers and arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,6 +3,15 @@
  * _memcpy - function that copies memory area
  * @dest: destiny character
  * @src: source character
- * @n: 
+ * @n: number of bytes
  * Return: Nothing.
  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int contador;
+
+	for (contador = 0; contador < n; contador++)
+		dest[contador] = src[contador];
+	return (dest);
+}
