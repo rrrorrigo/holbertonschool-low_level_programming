9959e5ab4518fb617e64ca6cd4b20a29ffac6877
rrrorrigo <rodrigobriones@gmail.com>
2021-02-24 19:52:35 +0000

    hotfix

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -7,20 +7,12 @@
  */
 int is_PalRec(char *str, int s, int e)
 {
-
-	// If there is only one character
 	if (s == e)
 		return (1);
 
-	// If first and last
-	// characters do not match
 	if (str[s] != str[e])
 		return (0);
 
-	// If there are more than
-	// two characters, check if
-	// middle substring is also
-	// palindrome or not.
 	if (s < e + 1)
 		return (is_PalRec(str, s + 1, e - 1));
 
@@ -30,11 +22,5 @@ int is_PalRec(char *str, int s, int e)
 int is_palindrome(char *s)
 {
 	int n = _strlen_recursion(s);
-
-	// An empty string is
-	// considered as palindrome
-	if (n == 0)
-		return (1);
-
 	return (is_PalRec(s, 0, n - 1));
 }
