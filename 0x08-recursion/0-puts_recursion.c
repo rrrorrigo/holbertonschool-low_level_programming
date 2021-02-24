e5f32fcaf17e87f8279753d44c8c712e12ec0622
rrrorrigo <rodrigobriones@gmail.com>
2021-02-24 11:44:26 +0000

    task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *
+ * @s: source string
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+		_puts_recursion(s);
+	else
+		_putchar(10);
+}
