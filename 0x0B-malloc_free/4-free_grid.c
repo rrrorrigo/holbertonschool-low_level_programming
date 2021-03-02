06f46d27f8ec856893ee26513104557c43dcce34
rrrorrigo <rodrigobriones@gmail.com>
2021-03-02 20:21:14 +0000

    free memory

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array - prints buffer in hexa
- * @size: te size of the memory to copy
- * @c: the char to copy
+ * free_grid - frees bidimensional grid
+ * @grid: te size of the memory to copy
+ * @height: the char to copy
  *
- * Return: char.
  */
 void free_grid(int **grid, int height)
 {
 	int Row = 0;
+
 	for (Row =0; Row < height; Row++)
 	{
 		free(grid[Row]);
