b68feb1713fb15ba514647c5b78427e301b3ce6a
rrrorrigo <rodrigobriones@gmail.com>
2021-03-02 16:34:38 +0000

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,15 @@ char *_strdup(char *str)
 	char *strdup;
 	int i, ii;
 
+	if (str == NULL)
+		return (NULL);
 	for (ii = 0; str[ii] !='\0'; ii++)
 	{
 	}
 	strdup = malloc(ii);
+	if (strdup == NULL)
+                return (NULL);
 	for (i = 0; i < ii; i++)
 		strdup[i] = str[i];
-	if (str == NULL || strdup == NULL)
-		return (NULL);
 	return (strdup);
 }
