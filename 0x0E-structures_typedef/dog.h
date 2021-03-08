26ffc5327f855206046b4d1baac717c34533e6df
rrrorrigo <rodrigobriones@gmail.com>
2021-03-08 17:20:23 +0000

    commit fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H
 #define _DOG_H
+/**
+ * struct dog - structure of dog information
+ * @name: name of dog
+ * @age: age of dof
+ * @owner: name of the owner of the dog
+ */
 struct dog
 {
 	char *name;
