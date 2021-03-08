8832c80c4568491a5dbdd13c97e7c6e3db35c51d
rrrorrigo <rodrigobriones@gmail.com>
2021-03-08 20:43:24 +0000

    free the dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -7,5 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
+	if (d)
+	{
+		free((*d).name);
+		free((*d).owner);
+	}
 	free(d);
 }
