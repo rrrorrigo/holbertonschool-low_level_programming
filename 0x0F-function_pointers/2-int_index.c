82246615ac254ab3b2153b4b00d221710286f634
rrrorrigo <rodrigobriones@gmail.com>
2021-03-10 16:00:25 +0000

    Int index and hot fix

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that search an int
+ * @array: element to print in the cmp function
+ * @size: size of array
+ * @cmp: name of function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+	for (int i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
