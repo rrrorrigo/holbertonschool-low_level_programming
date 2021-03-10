dd24a672e9ec0dcaa581cfd060eddc919aaac4d7
rrrorrigo <rodrigobriones@gmail.com>
2021-03-10 22:19:21 +0000

    print simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,8 +18,8 @@ int (*get_op_func(char *s))(int a, int b)
 	int i = 0;
 	while (ops[i].op)
 	{
-		if (ops[i].op == s)
-			return(ops[i].f(a, b))
+		if (*(ops[i].op) == *s)
+			return(ops[i].f);
 		i++;
 	}
 	return (NULL);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,25 +1,30 @@
 #include "3-calc.h"
-#include <stdio.h>
 /**
  * main - main function
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (operator)(int, int);
+	int res;
+	int (*operation)(int a, int b);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	operator = get_op_func(argv[2]);
-	if (operator == NULL)
+	operation = get_op_func(argv[2]);
+	if (!operation || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+if ((atoi(argv[2]) == '/' || atoi(argv[2]) == '%') && atoi(argv[3]) == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	res = operation(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", res);
+	return (0);
 }
