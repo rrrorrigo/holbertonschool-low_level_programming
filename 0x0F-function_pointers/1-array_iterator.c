5317d22004e3c310eaf33cf339c373c7fb9b8e05
rrrorrigo <rodrigobriones@gmail.com>
2021-03-10 16:05:39 +0000

    hot fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array && size && action)
+	if (action)
 		for (unsigned int i = 0; i < size; i++)
 			action(array[i]);
 }
