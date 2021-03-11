1e8129c2ac3ee3e7b08b2c96706af80c1362e3a6
rrrorrigo <rodrigobriones@gmail.com>
2021-03-11 17:24:55 +0000

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 
 	va_start(up, n);
-	if (separator != NULL || n > 0)
+	if (separator != 'NULL')
 	{
 		for (; i < n; i++)
 		{
