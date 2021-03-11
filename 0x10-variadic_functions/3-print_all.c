f0fd997574241b881a800564e6a72f36b4c00130
rrrorrigo <rodrigobriones@gmail.com>
2021-03-11 18:09:56 +0000

    incomplete

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - print anything.
+ * @format: type of argument passed
+ * @...: parameters value
+ */
+void print_all(const char * const format, ...)
+{
+	va_list up;
+	int index = 0, c, i, f, s;
+
+	while (format[index] != '\0')
+	{
+
+		index++;
+	}
+
+}
