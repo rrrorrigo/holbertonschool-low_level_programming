9f94f8c6f9f5a933c1ffaa44e25d42cbd8bb7a7c
rrrorrigo <rodrigobriones@gmail.com>
2021-03-12 01:05:08 +0000

    printf fake

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,12 +8,74 @@
 void print_all(const char * const format, ...)
 {
 	va_list up;
-	int index = 0, c, i, f, s;
+	int i, ii = 0;
+	char *espacio = "";
+	tipo formato[] = {
+		{"c", _char},
+		{"i", _int},
+		{"f", _float},
+		{"s", _string},
+		{NULL, NULL}
+	};
 
-	while (format[index] != '\0')
+	va_start(up, format);
+	while (format && format[ii] != '\0')
 	{
-
-		index++;
+		i = 0;
+		while (i < 4)
+		{
+			if (*(formato[i].c) == format[ii])
+			{
+				printf("%s", espacio);
+				formato[i].ptrfunc(up);
+				espacio = ", ";
+			}
+			i++;
+		}
+		ii++;
 	}
+	printf("\n");
+	va_end(up);
+}
+/**
+ * _char - print a char
+ *
+ * @up: type of va_list that be printed
+ */
+void _char(va_list up)
+{
+	printf("%c", va_arg(up, int));
+}
 
+/**
+ * _int - print a integer
+ *
+ * @up: type of va_list that be printed
+ */
+void _int(va_list up)
+{
+	printf("%d", va_arg(up, int));
+}
+/**
+ * _float - print a float
+ *
+ * @up: type of va_list that be printed
+ */
+void _float(va_list up)
+{
+	printf("%f", va_arg(up, double));
+}
+/**
+ * _string - print a string
+ *
+ * @up: type of va_list that be printed
+ */
+void _string(va_list up)
+{
+	char *check;
+
+	check = va_arg(up, char *);
+	if(!check)
+		check = "(nil)";
+	printf("%s", check);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,18 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+
+void _char(va_list up);
+void _int(va_list up);
+void _float(va_list up);
+void _string(va_list up);
+
 typedef struct tipo
 {
-	char *format;
-	void *(ptrfunc)(va_list);
-}
+	char *c;
+	void (*ptrfunc)(va_list);
+}tipo;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
