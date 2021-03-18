50219b08b6d9ad713c775c617cd085fb0d2b1c73
Rodrigo <rodrigobriones54@gmail.com>
2021-03-18 20:15:53 +0000

    add a new node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,20 @@
 #include "lists.h"
 /**
- * list_len - function that return the number of elements
- * @h: pointer to it struct
+ * add_node - function that add a new node at the beginning
+ * @head: pointer to the struct
+ * @str: element to add on this new node
  * Return: the number of nodes
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode = malloc(sizeof(list_t));
+
 	if (!newNode)
 		return (NULL);
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
 	newNode->next = *head;
 	*head = newNode;
+
 	return (newNode);
 }
