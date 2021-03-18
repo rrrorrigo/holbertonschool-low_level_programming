e96def1777f1edc84c2a3ef17ef175d7f862ae2d
Rodrigo <rodrigobriones54@gmail.com>
2021-03-18 22:01:23 +0000

    advanced task, print sentence before a main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- *
- *
+ * before - print a string before the main function
  */
-void before(void) __attribute__ ((constructor));
-
-void before(void)
+void __attribute__ ((constructor)) before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
