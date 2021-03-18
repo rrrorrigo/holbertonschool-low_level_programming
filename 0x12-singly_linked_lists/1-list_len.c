d01fd4a1b9b51b2573ec16512b61efc69e528609
Rodrigo <rodrigobriones54@gmail.com>
2021-03-18 19:38:37 +0000

    print the number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - function that return the number of elements
+ * @h: pointer to it struct
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h);
+{
+	size_t nodo = 0;
+
+	while (h)
+	{
+		nodo++;
+		h = h->next;
+	}
+	return (nodo);
+}
