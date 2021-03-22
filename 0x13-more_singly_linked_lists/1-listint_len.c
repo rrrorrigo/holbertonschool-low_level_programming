f3785450536182052678eda6bf4396837ce08f5c
Rodrigo <rodrigobriones54@gmail.com>
2021-03-22 13:38:51 +0000

    print the number of elements linked

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - function that return the number of elements
+ * @h: pointer to it struct
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodo = 0;
+
+	while (h)
+	{
+		nodo++;
+		h = h->next;
+	}
+	return (nodo);
+}
