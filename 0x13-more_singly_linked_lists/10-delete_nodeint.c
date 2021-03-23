c57bec6d9154c57d45433eb859578395aca5a565
Rodrigo <rodrigobriones54@gmail.com>
2021-03-23 19:16:06 +0000

    check 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		head = &(*head)->next;
 		i++;
 	}
-	if (*head)
+	if (*head && i == index)
 	{
 		auxNode = *head;
 		*head = (*head)->next;
