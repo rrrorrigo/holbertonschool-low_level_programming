d8a5367eab2d129c1ec87102266f3cbf4664ae94
Rodrigo <rodrigobriones54@gmail.com>
2021-03-29 23:06:07 +0000

    betty style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -59,7 +59,7 @@ int main(int argc, char **argv)
 	}
 	if (characters < 0)
 		error_read(argv[1]);
-	if (close(file_from) < 0 )
+	if (close(file_from) < 0)
 		error_close(file_from);
 	if (close(file_to) < 0)
 		error_close(file_to);
