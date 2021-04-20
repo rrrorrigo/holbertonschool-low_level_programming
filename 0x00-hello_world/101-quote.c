12409ebd5115282535943d463c41d82cdacfb59b
Rodrigo <rodrigobriones54@gmail.com>
2021-04-20 21:25:32 +0000

    advanced task, print a text with write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - function that print a text with write function
+ * Return: Always 1.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+return (1);
+}
