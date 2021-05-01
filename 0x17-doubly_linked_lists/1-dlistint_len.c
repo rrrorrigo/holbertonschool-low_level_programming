d90d907077c5515af9b95552e3dde1e6279ac3f1
Rodrigo <rodrigobriones54@gmail.com>
2021-05-01 19:18:43 +0000

    count the content of the linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - function that count the content on the linked list
+ * @h: pointer to the linked list
+ * Return: the number of values counted
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t con = 0;
+
+        while (h)
+        {
+                h = h->next;
+                con++;
+        }
+        return (con);
+}
