3adb1b2d02698306fbacf8656757bf3f495b54b5
Rodrigo <rodrigobriones54@gmail.com>
2021-05-28 23:39:35 +0000

    print hash tabel

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,21 +5,23 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-        unsigned long int i = ht->size, ii = 0;
-        short int aux = 1;
+	unsigned long int i = ht->size, ii = 0;
+	short int aux = 1;
 	char *key, *value;
 
+	if (!ht)
+		return;
 	putchar('{');
-        for(; ii < i; ii++)
-                if(ht->array[ii] != NULL)
-                {
+	for (; ii < i; ii++)
+		if (ht->array[ii] != NULL)
+		{
 			key = ht->array[ii]->key;
 			value = ht->array[ii]->value;
-                        if (aux)
-                                printf("\'%s\': \'%s\'", key, value);
-                        else
-                                printf(", \'%s\': \'%s\'", key, value);
-                        aux = 0;
-                }
-        printf("}\n");
+			if (aux)
+				printf("\'%s\': \'%s\'", key, value);
+			else
+				printf(", \'%s\': \'%s\'", key, value);
+			aux = 0;
+		}
+	printf("}\n");
 }
