e9d647bcd5639024bbdb428f5c25e0fd421d7867
Rodrigo <rodrigobriones54@gmail.com>
2021-05-28 14:23:25 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new;
 	unsigned long int i = 0;
 
-	new = malloc(sizeof(hash_table_t));
+	new = calloc(size, sizeof(hash_table_t));
 	if (!new)
 		return NULL;
 	new->size = size;
-	new->array = malloc(sizeof(hash_table_t *) * size);
+	new->array = calloc(size, sizeof(hash_table_t *) * size);
 	if (!new->array)
 	{
 		free(new);
 		return NULL;
-	}
-	while (i < size)
-	{
-		new->array = NULL;
-		i++;
 	}
 	return new;
 }
