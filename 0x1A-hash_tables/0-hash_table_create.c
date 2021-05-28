4fc0a40c74a1ffd66737be3af7680e14f08868b4
Rodrigo <rodrigobriones54@gmail.com>
2021-05-28 14:46:19 +0000

    betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
- * hash_table_t - function that creates a hash table
- * @size: size of the arra
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
  * Return: pointer to newly created hash table, if error return NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -9,16 +9,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	new = calloc(1, sizeof(hash_table_t));
 	if (!new)
-		return NULL;
+		return (NULL);
 	new->size = size;
 	new->array = calloc(size, sizeof(hash_table_t **));
 	if (!new->array)
 	{
 		free(new);
-		return NULL;
+		return (NULL);
 	}
-	return new;
+	return (new);
 }
