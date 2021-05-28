ae252581a6c69f5072d47dd09d060cb28608553f
Rodrigo <rodrigobriones54@gmail.com>
2021-05-28 14:40:13 +0000

    solving unknown error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = calloc(size, sizeof(hash_table_t));
+	if (size == 0)
+		return NULL;
+	new = calloc(1, sizeof(hash_table_t));
 	if (!new)
 		return NULL;
 	new->size = size;
-	new->array = calloc(size, sizeof(hash_table_t *) * size);
+	new->array = calloc(size, sizeof(hash_table_t *));
 	if (!new->array)
 	{
 		free(new);
