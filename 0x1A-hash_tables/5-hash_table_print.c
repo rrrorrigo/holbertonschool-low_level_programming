6c0f942e45473bbe2eac974ee782395a7132b0b8
Rodrigo <rodrigobriones54@gmail.com>
2021-05-29 00:12:44 +0000

    print the hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,21 +7,25 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = ht->size, ii = 0;
 	short int aux = 1;
-	char *key, *value;
+	hash_node_t *arr;
+	char *key, *val;
 
 	if (!ht)
 		return;
 	putchar('{');
 	for (; ii < i; ii++)
-		if (ht->array[ii] != NULL)
+	{
+		arr = ht->array[ii];
+		for (; arr; arr = arr->next)
 		{
 			key = ht->array[ii]->key;
-			value = ht->array[ii]->value;
+			val = ht->array[ii]->value;
 			if (aux)
-				printf("\'%s\': \'%s\'", key, value);
+				printf("\'%s\': \'%s\'", key, val);
 			else
-				printf(", \'%s\': \'%s\'", key, value);
+				printf(", \'%s\': \'%s\'", key, val);
 			aux = 0;
 		}
+	}
 	printf("}\n");
 }
