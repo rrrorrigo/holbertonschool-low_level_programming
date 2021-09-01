2677a4eccce99b91d50def8feaa4177fe341fe31
basic name <my@email.com>
2021-09-01 01:30:13 +0000

    fixed betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,20 +1,21 @@
 #include "search_algos.h"
 /**
- * linear_search - function that searches for a value in an array 
+ * linear_search - function that searches for a value in an array
  * of integers using the Linear search algorithm
  * @array: array to search data
  * @size: size of array
  * @value: value to search
+ * Return: -1 if error occurred, index of found value
  */
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        for (; i < size; i++)
-        {
-                printf("Value checked array[%u] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
-        }
-        return (-1);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,18 +2,19 @@
 /**
  * print - function to print array
  * @array: array to print
+ * @i: index
  * @size: size of array
  */
 void print(int *array, size_t i, size_t size)
 {
-        printf("Searching in array: ");
-        for (; i < size + 1; i++)
-        {
-                printf("%u", array[i]);
-                if (i < size)
-                        printf(", ");
-        }
-        putchar('\n');
+	printf("Searching in array: ");
+	for (; i < size + 1; i++)
+	{
+		printf("%u", array[i]);
+		if (i < size)
+			printf(", ");
+	}
+	putchar('\n');
 }
 /**
  * binary_search - function that searches for a value
@@ -21,23 +22,25 @@ void print(int *array, size_t i, size_t size)
  * @array: array to search a value
  * @size: size of array
  * @value: value to search on array
- * 
+ * Return: index where value was found or -1 if it failed
  */
 int binary_search(int *array, size_t size, int value)
 {
-        size_t i = 0, M;
+	size_t i = 0, M;
 
-        size = size - 1;
-        for (; i <= size;)
-        {
-                print(array, i, size);
-                M = (i + size) / 2;
-                if (array[M] == value)
-                        return (M);
-                if (array[M] < value)
-                        i = M + 1;
-                if (array[M] > value)
-                        size = M - 1;
-        }
-        return (-1);
+	size = size - 1;
+	if (!value)
+		return (-1);
+	for (; i <= size;)
+	{
+		print(array, i, size);
+		M = (i + size) / 2;
+		if (array[M] == value)
+			return (M);
+		if (array[M] < value)
+			i = M + 1;
+		if (array[M] > value)
+			size = M - 1;
+	}
+	return (-1);
 }
