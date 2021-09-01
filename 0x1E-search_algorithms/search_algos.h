086988433e6d4d7161539bb16088b5313703ab3b
basic name <my@email.com>
2021-09-01 01:51:34 +0000

    erase empty line

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,6 @@
 #define HOLBERTON_H
 #include <stdio.h>
 #include <stdlib.h>
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
