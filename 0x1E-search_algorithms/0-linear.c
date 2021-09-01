75b01d5f910081302835ad0507d05b06ca16fa9d
basic name <my@email.com>
2021-09-01 01:46:25 +0000

    added border case

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,6 +11,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if (!array)
+		return (-1);
 	for (; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
